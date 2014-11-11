6ba429469dae7b9abaf3e5db9b320f01a0055533
William Prin <waprin@gmail.com>
2014-11-11 14:12:29 -0500

    Use redis cache instead of redis

--- league/settings.py
+++ league/settings.py
@@ -226,10 +226,11 @@ LOGGING = {
 }
 
 redis_url = urlparse.urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost:6379'))
+#LOCAL_REDIS='redis://localhost:6379'
 
 CACHES = {
     'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
+        'BACKEND': 'redis_cache.RedisCache',
         'LOCATION': '%s:%s' % (redis_url.hostname, redis_url.port),
         'OPTIONS': {
             'DB': 0,
