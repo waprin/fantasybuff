a9a9d1877a9bd1fe4f82c8601df4db14d6a4f979
William Prin <waprin@gmail.com>
2014-11-28 13:33:31 -0500

    Missing Migrations In Last Commit

---
+++ teams/migrations/0021_espnuser_calculating.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('teams', '0020_auto_20141126_2006'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='espnuser',
+            name='calculating',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+    ]
