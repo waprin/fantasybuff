0e8f2b79262c5e6a5b66f2557e8931e6593703c8
William Prin <waprin@gmail.com>
2014-12-02 13:27:36 -0500

    Fix weeknum range

--- teams/scraper/utils.py
+++ teams/scraper/utils.py
@@ -6,7 +6,7 @@ __author__ = 'bprin'
 def real_num_weeks():
     start = datetime.datetime(year=2014, month=9, day=9)
     week = datetime.timedelta(days=7)
-    start_days = [start + (weeknum * week) for weeknum in range(0, 13)]
+    start_days = [start + (weeknum * week) for weeknum in range(0, 17)]
     now = datetime.datetime.now()
     for i in range(0, 17):
         if now < start_days[i]:
